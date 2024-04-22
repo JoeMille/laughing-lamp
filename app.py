545b2090c7be5fc43160db976ca886628d1008ad
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2024-04-22 17:42:56 +0100

    End session

--- app.py
+++ app.py
@@ -1,30 +1,58 @@
-import os
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.utils import secure_filename
-from models import db, User
-from forms import LoginForm
-from dotenv import load_dotenv
-
-load_dotenv()
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.db'
+app.config['SECRET_KEY'] = 'your-secret-key'
 
-db.init_app(app)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 with app.app_context():
     db.create_all()
 
-@app.route ('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and user.password == form.password.data:
+    registration_form = RegistrationForm(request.form)
+    login_form = LoginForm(request.form)
+    if request.method == 'POST':
+        if registration_form.validate_on_submit():
+            hashed_password = generate_password_hash(registration_form.password.data, method='sha256')
+            new_user = User(username=registration_form.username.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User has been successfully registered!', 'success')
             return redirect(url_for('home'))
-    return render_template('index.html', form=form)
+        elif login_form.validate_on_submit():
+            user = User.query.filter_by(username=login_form.username.data).first()
+            if user and check_password_hash(user.password, login_form.password.data):
+                return redirect(url_for('home'))
+            else:
+                return 'Invalid username or password'
+    return render_template('index.html', login_form=login_form, registration_form=registration_form)
+    
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
